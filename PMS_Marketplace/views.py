9faa69b1574bcd52fe72b2e31a41a40253756d4c
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-04-05 07:51:35 +0200

    half the way there

--- PMS_Marketplace/views.py
+++ PMS_Marketplace/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "home.html")
+
+def cart(request):
+    return render(request, "cart.html")
+
+def category(request, name):
+    name = name.replace("_", " ").title()
+    return render(request, "category.html", {
+        "category_name": name
+    })
