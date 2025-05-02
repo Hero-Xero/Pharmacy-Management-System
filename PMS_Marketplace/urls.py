72449701902d046f56cf6bff42cbdfedf2064188
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-05-03 02:00:04 +0300

    nearly done

--- PMS_Marketplace/urls.py
+++ PMS_Marketplace/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("cart/", views.cart, name="cart"),
     path("category/<str:name>", views.category, name="category"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("update_cart_item/<int:cart_item_id>/", views.update_cart_item, name="update_cart_item"),
+    path("add_to_cart/<int:product_id>/", views.add_item_to_cart, name="add_item"),
 ]
