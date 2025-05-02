e50c6fa6f991b893d3a9f09bb47b202f9c4fb5cc
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-05-02 22:41:34 +0300

    finished the admin and client permissions

--- PMS_Admin/views.py
+++ PMS_Admin/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+from PMS_Accounts.decorators import admin_required
 from django.shortcuts import render
 
 @login_required
+@admin_required
 def dashboard_view(request):
     # In a real app, you might pull these stats from your database
     context = {
