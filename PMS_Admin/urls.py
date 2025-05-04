1c37e5df771cd39ebc958a33a2342fa92ee74721
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-05-04 18:54:22 +0300

    updating the structure.

--- PMS_Admin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "dashboard"
-urlpatterns = [
-    path("", views.dashboard_view, name="home"),
-    path("add_product/", views.add_product, name="add_product"),
-    path("add_category/", views.add_category, name="add_category")
-]
--- PMS_Marketplace/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PmsMarketplaceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PMS_Marketplace'
